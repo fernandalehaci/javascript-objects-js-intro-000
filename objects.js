69c37dc9d444cc53aaf8563d109b2942ee780042
Fernanda Lehaci <fernanda.lehaci@gmail.com>
2019-12-11 00:24:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist,artistName ,songTitle){
   return newPlaylist
 }
 function removeFromPlaylist(playlist,artistName){
-  delete playlist.artistName
+  delete playlist[artistName]
   return playlist
 }
