80f0a95e68c85e43d3b9abf322835fd87d65166b
Fernanda Lehaci <fernanda.lehaci@gmail.com>
2019-12-11 00:23:24 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,9 @@
+var playlist= {artist:"songName"}
+function updatePlaylist(playlist,artistName ,songTitle){
+  var newPlaylist= Object.assign(playlist,{[artistName] : songTitle})
+  return newPlaylist
+}
+function removeFromPlaylist(playlist,artistName){
+  delete playlist.artistName
+  return playlist
+}
